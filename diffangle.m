3747b6bda8d81abb156262eb1fceeaf221dacdc9
Xiaobo Liu <95089921+Laoma29@users.noreply.github.com>
2025-11-08 18:18:59 +0800

    Add files via upload

--- diffangle.m
+++ diffangle.m
@@ -1,10 +1,3 @@
 function theta = diffangle(theta2, theta1)
-  theta = theta2 - theta1;
-  for ii=1:length(theta)
-    if theta(ii) < -pi
-      theta(ii) = theta(ii) + 2*pi;
-    elseif theta(ii) > pi
-        theta(ii) = theta(ii) - 2*pi;
-    end
-  end
+％％used matlab function instead of
 end
